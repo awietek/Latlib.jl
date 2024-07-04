8105e9683cc571d7fe5241137175a745fbe7dacf
Documenter.jl <documenter@juliadocs.github.io>
2024-07-04 12:29:05 +0000

    build based on 53e5bfd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Latlib-Documentation","page":"Latlib Documentation","title":"Latlib Documentation","text":"","category":"section"},{"location":"","page":"Latlib Documentation","title":"Latlib Documentation","text":"Lattice","category":"page"},{"location":"#Latlib.Lattice","page":"Latlib Documentation","title":"Latlib.Lattice","text":"Lattice\n\nA lattice is defined by its Bravais lattice,  basis coordinates and optionally the type of  the basis lattice sites.\n\nThe Bravais lattice is defined at a set of points\n\nmathbfR = sum_i=1^D n_i mathbfa_i\n\nwhere D denotes the dimension of the lattice,  n_i are integers and mathbfa_i are the Bravais vectors. \n\n\n\n\n\n","category":"type"}]
+}
