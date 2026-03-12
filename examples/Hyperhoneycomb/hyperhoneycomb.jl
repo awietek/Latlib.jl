57ba9f4cb91b969b7b4261b18f8f3ee5685ebea9
Paul Ebert <paul.ebert@mail.de>
2026-03-12 15:45:02 +0100

    added N=16,32 hyperhoneycomb files to examples including plotting

--- examples/Hyperhoneycomb/hyperhoneycomb.jl
+++ examples/Hyperhoneycomb/hyperhoneycomb.jl
@@ -2,12 +2,32 @@ using Revise
 using Latlib
 
 # Hyperhoneycomb: 3D lattice with 4 atoms per unit cell
-fl = FiniteLattice(hyperhoneycomb, [2 0 0; 0 2 0; 0 0 2])
+flat_2x2 = FiniteLattice(hyperhoneycomb, [2 0 0; 0 2 0; 0 0 2])
+#plot_3d(flat_2x2; show_neighbors=true, show_unit_cell=true)
 
-#plot_3d(fl; show_neighbors=true, show_unit_cell=true)
+# N=16 cluster
+flat_16_vecs = [
+    LatticeVector(hyperhoneycomb, [-1, 1, 1]),  # t1
+    LatticeVector(hyperhoneycomb, [1, 1, -1]),  # t2
+    LatticeVector(hyperhoneycomb, [-1, 1, -1]), # t3
+]
 
-# define Heisenberg interactions between nearest neighbors
+flat_16 = FiniteLattice(flat_16_vecs, true)
+#plot_3d(flat_16; show_neighbors=true, show_unit_cell=true)
+
+
+# N=32 cluster
+flat_32_vecs = [
+    LatticeVector(hyperhoneycomb, [-1, 1, 1]), # t1
+    LatticeVector(hyperhoneycomb, [1, 1, -1]), # t2
+    LatticeVector(hyperhoneycomb, [-2, 1, -2]),# t3
+]
+flat_32 = FiniteLattice(flat_32_vecs, true)
+plot_3d(flat_32; show_neighbors=true, show_unit_cell=true)
+
+
+
+# print to stdout what would be written to TOML file
 opsum = neighbor_interaction("SdotS", "J", fl)
-
-toml_string = write_toml(fl, opsum, "placeholder"; index_zero=true, return_string=true)
-println(toml_string)
+toml_string = write_toml(fl, opsum, "placeholder"; zero_based=true, return_string=true)
+#println(toml_string)
